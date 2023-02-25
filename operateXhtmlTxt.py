a628c6f15a03f11cb07846728260568391ccd7e6
vert2 <vert2air@gmail.com>
2023-02-25 20:08:01 +0900

    add : Support multi tags
    Changes to be committed:
    	modified:   operateXhtmlTxt.py

--- operateXhtmlTxt.py
+++ operateXhtmlTxt.py
@@ -14,7 +14,6 @@ def extract(soup, tag_name, repl=[]) :
         replaced = False
         tags = soup.select(tag_name)
         cidx = 0
-        print('-- while loop --- tidx:{} repl.len:{}'.format(tidx, len(repl)))
         for i, tg in enumerate(tags) :
             for j, ct in enumerate(tg.contents) :
                 if type(ct) is NavigableString :
@@ -31,26 +30,6 @@ def extract(soup, tag_name, repl=[]) :
                     else :
                         ans.extend( xs )
                     cidx += 1
-                elif type(ct) is Tag and ct.name == 'a' :
-                    # <tag_name>タグの内容として、<a>タグが含まれていた。
-                    for k, c_in_a in enumerate(ct.contents) :
-                        # <tag_name>タグ内の<a>タグの内容check
-                        if type(c_in_a) is NavigableString :
-                            xs = useCp932(str(c_in_a)).split('\n')
-                            if len(repl) > 0 :
-                                if cidx == tidx :
-                                    lcnt = len(xs)
-                                    ct.contents[k].replace_with(
-                                                        '\n'.join(repl[ :lcnt]))
-                                    del repl[ : lcnt]
-                                    replaced = True
-                                    cidx += 1
-                                    break
-                            else :
-                                ans.extend( xs )
-                            cidx += 1
-                    if tidx >= 0 and cidx > tidx :
-                        break
                 if tidx >= 0 and cidx > tidx :
                     break
             if tidx >= 0 and cidx > tidx :
@@ -58,24 +37,25 @@ def extract(soup, tag_name, repl=[]) :
         if repl == [] or not replaced :
             break
         tidx += 1
-        if tidx > 1050 :
-            break
     return ans, soup
 
 fnSource = sys.argv[1]
 fnXlated = sys.argv[2] if len(sys.argv) > 2 else ''
 
+tags = [ 'p', 'a', 'h3' ]
 with open(fnSource, 'r', encoding='utf8') as fs :
     xhtml = fs.read()
 soup = BeautifulSoup(xhtml, "html.parser")
 if fnXlated == '' :
-    ans, _ = extract(soup, 'p')
-    for a in ans :
-        print(a)
+    for tag in tags :
+        ans, _ = extract(soup, tag)
+        for a in ans :
+            print(a)
 else :
     with open(fnXlated, 'r', encoding='utf8') as fx :
-        xlated = fx.read().split('\n')
-    _, soup = extract(soup, 'p', repl=xlated)
+        xlated = fx.readlines()
+    for tag in tags :
+        _, soup = extract(soup, tag, repl=xlated)
     with open('out.xhtml', 'w', encoding='utf8') as fw :
         #fw.write(soup.prettify(formatter='minimal'))
         fw.write(soup.prettify(formatter='html'))
